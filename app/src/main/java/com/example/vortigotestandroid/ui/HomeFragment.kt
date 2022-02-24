4dced237362baf02d54a3d364b2decabaed1f578
Paulo Víctor Corazza <paulo.corazza@vortigo.com.br>
2022-02-24 09:57:38 -0300

    Attach fragment to activity and inflate fragment view with DataBinding

--- app/src/main/java/com/example/vortigotestandroid/ui/HomeFragment.kt
+++ app/src/main/java/com/example/vortigotestandroid/ui/HomeFragment.kt
@@ -5,17 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.vortigotestandroid.R
+import com.example.vortigotestandroid.databinding.FragmentHomeBinding
 
 // TODO: Popular a UI com as informações recuperadas do serviço
 
 class HomeFragment : Fragment() {
 
+    private lateinit var binding: FragmentHomeBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root
     }
 
 }
